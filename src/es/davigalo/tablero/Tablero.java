c064c11d0e431259333d7cd9794fc6aa3af7a50d
user.email <davigalo2000@gmail.com>
2024-12-16 12:40:44 +0100

    Posiciones iniciales de las piezas completadas

--- src/es/davigalo/tablero/Tablero.java
+++ src/es/davigalo/tablero/Tablero.java
@@ -11,13 +11,33 @@ public class Tablero {
     for (i = 0; i < NUM_FILAS; i++) {
       for (j = 0; j < NUM_COLUMNAS; j++) {
         if (i == 0 && j == 0 || i == 0 && j == NUM_COLUMNAS-1) {
-          tablero[i][j] = " T2 ";
+          tablero[i][j] = "T2";
         } else if (i == NUM_FILAS-1 && j == 0 || i == NUM_FILAS-1 && j == NUM_COLUMNAS-1) {
-          tablero[i][j] = " T1 ";
+          tablero[i][j] = "T1";
         
+        }else if (i == 1) {
+          tablero[i][j] = "P2";
+        }else if (i == 6) {
+          tablero[i][j] = "P1";
+        }else if (i == 0 && j == 1 || i == 0 && j == 6) {
+          tablero[i][j] = "C2";
+        }else if (i == NUM_FILAS-1 && j == 1 || i == NUM_FILAS-1 && j == 6) {
+          tablero[i][j] = "C1";
+        }else if (i == 0 && j == 2 || i == 0 && j == 5) {
+          tablero[i][j] = "A2";
+        }else if (i == NUM_FILAS-1 && j == 2 || i == NUM_FILAS-1 && j == 5) {
+          tablero[i][j] = "A1";
+        }else if (i == 0 && j == 3 ) {
+          tablero[i][j] = "D2";
+        }else if (i == NUM_FILAS-1 && j == 3) {
+          tablero[i][j] = "D1";
+        }else if (i == 0 && j == 4 ) {
+          tablero[i][j] = "R2";
+        }else if (i == NUM_FILAS-1 && j == 4 ) {
+          tablero[i][j] = "R1";
         }
         else {
-        tablero[i][j] = " * ";
+        tablero[i][j] =  "*";
         }
         }
     }
@@ -28,7 +48,7 @@ public class Tablero {
     int i, j;
     for (i = 0; i < NUM_FILAS; i++) {
       for (j = 0; j < NUM_COLUMNAS; j++) {
-        System.out.print(tablero[i][j]);
+        System.out.print("\t"+tablero[i][j]);
       }
       System.out.print("\n");
     }
